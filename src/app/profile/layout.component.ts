35c79e83c5e2c76a8ca00804f487add305cb292c
Jerson Villegas <ksbashelor@gmail.com>
2026-05-14 17:08:22 +0800

    fix for vercel

--- src/app/profile/layout.component.ts
+++ src/app/profile/layout.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-profile-layout',
-  templateUrl: './Layout.component.html',
+  templateUrl: './layout.component.html',  // ✅ lowercase l
   standalone: false
 })
 export class LayoutComponent {}
