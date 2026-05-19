0bac612595e7a03bd1e1c877b546a2964278fbfa
ItsCardo05 <ricardo.bajejr19@gmail.com>
2026-05-19 17:17:34 +0300

    update api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://lab-6-activity-building-a-node-js.onrender.com'
+  apiUrl: 'https://node-mysql-api-00an.onrender.com/accounts'
 };
