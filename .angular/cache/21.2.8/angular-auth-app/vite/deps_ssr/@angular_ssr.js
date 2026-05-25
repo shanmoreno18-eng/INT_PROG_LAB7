69f38d83833eeb6ee2ad3737eef44cc571ab9997
Shantal Moreno Cabutaje <Shantal Moreno Cabutaje>
2026-05-26 00:18:59 +0800

    initial commit - Lab7 Activity

---
+++ .angular/cache/21.2.8/angular-auth-app/vite/deps_ssr/@angular_ssr.js
@@ -0,0 +1,44 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  AngularAppEngine,
+  IS_DISCOVERING_ROUTES,
+  InlineCriticalCssProcessor,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  destroyAngularServerApp,
+  extractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig,
+  provideServerRendering,
+  setAngularAppEngineManifest,
+  setAngularAppManifest,
+  withAppShell,
+  withRoutes
+} from "./chunk-NVBHII7J.js";
+import "./chunk-WC4PITUE.js";
+import "./chunk-K7I5XBZ6.js";
+import "./chunk-YEENNMZU.js";
+import "./chunk-TXNAHYJT.js";
+import "./chunk-DMZWARZJ.js";
+import "./chunk-C27DBZK2.js";
+import "./chunk-2UVUUPPC.js";
+import "./chunk-K54IFBYX.js";
+import "./chunk-6DU2HRTW.js";
+export {
+  AngularAppEngine,
+  IS_DISCOVERING_ROUTES,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  provideServerRendering,
+  withAppShell,
+  withRoutes,
+  InlineCriticalCssProcessor as ɵInlineCriticalCssProcessor,
+  destroyAngularServerApp as ɵdestroyAngularServerApp,
+  extractRoutesAndCreateRouteTree as ɵextractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp as ɵgetOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig as ɵgetRoutesFromAngularRouterConfig,
+  setAngularAppEngineManifest as ɵsetAngularAppEngineManifest,
+  setAngularAppManifest as ɵsetAngularAppManifest
+};
